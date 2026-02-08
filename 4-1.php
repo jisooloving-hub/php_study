0700b3d6475315d4d30dcee711faadf2d63177c4
jisoo <jisooloving@g.shingu.ac.kr>
2026-02-08 21:14:36 +0900

    4-1예제

---
+++ 4-1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <?php
+    echo "echo 사용예제<br>";
+    echo "시작     끝<br>";
+    
+    echo "계산 결과 출력 : ";
+    echo 3+7;
+    echo "<br>";
+
+    echo "한 줄의 echo로 여러 값 출력 : ", 3+7, "<br>";
+
+    echo "특수한 글자 출력 : \" \$<br>";
+    ECHO "대문자 ECHO도 똑같이 사용 가능<br>";
+    ?>
+</body>
+</html>
