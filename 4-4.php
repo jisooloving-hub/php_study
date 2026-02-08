a085702211cc95e0d9427bc6e126adf8a6550700
jisoo <jisooloving@g.shingu.ac.kr>
2026-02-08 21:50:26 +0900

    09:50

---
+++ 4-4.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>전달받은 값의 사용</title>
+</head>
+<body>
+    아이디 : <?= $_REQUEST["id"] ?><br>
+    비밀번호 : <?= $_REQUEST["pw"] ?><br>
+</body>
+</html>
